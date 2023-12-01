02553432bb1ffe41c1c8d1f61dc62a70cdeb2731
S-Filip <100999946+S-Filip@users.noreply.github.com>
2023-12-01 04:53:11 +0100

    Layer flattening
    Flattens the layer into one dimension so it can be processed by further layers; redesigned comments to be cleaner

--- digits.py
+++ digits.py
@@ -3,8 +3,16 @@ import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow as tf
 
-mnist = tf.keras.datasets.mnist # Dataset of 60,000 handwritten digits
-(x_train, y_train), (x_test, y_test) = mnist.load_data() # Loads and splits data (90:10)
+# Imports a dataset of 60,000 handwritten digits
+mnist = tf.keras.datasets.mnist
+# Loads and splits data in a 9:1 ratio
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-x_train = tf.keras.utils.normalize(x_train, axis=1) # Scales down x data to 0-1
+# Scales down x_train and x_test data to 0-1
+x_train = tf.keras.utils.normalize(x_train, axis=1)
 x_test = tf.keras.utils.normalize(x_test, axis=1)
+
+# Layers are stacked sequentially, taking the output of the previous one as input
+model = tf.keras.models.Sequential()
+# Flattens the layer into one dimension, expecting an input array of dimensions 28x28 pixels
+model.add(tf.keras.layers.Flatten(input_shape=(28,28)))
