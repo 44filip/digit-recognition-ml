ac2ba9968d2c15524d887d8268b3411456805bcd
S-Filip <100999946+S-Filip@users.noreply.github.com>
2023-12-01 04:43:08 +0100

    Loading dataset
    Importing a handwritten digits dataset into the app, then dividing data into training (90%) and testing (10%) data.

--- digits.py
+++ digits.py
@@ -2,3 +2,6 @@ import cv2
 import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow as tf
+
+mnist = tf.keras.datasets.mnist # Dataset of 60,000 handwritten digits
+(x_train, y_train), (x_test, y_test) = mnist.load_data() # Loads and splits data (90:10)
