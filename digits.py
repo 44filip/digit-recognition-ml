aa86724b44c228b5dfc4e9ee621d9006ab31c29a
S-Filip <100999946+S-Filip@users.noreply.github.com>
2023-12-01 04:45:39 +0100

    Normalizing x values
    Scaling down x values to 0-1.

--- digits.py
+++ digits.py
@@ -5,3 +5,6 @@ import tensorflow as tf
 
 mnist = tf.keras.datasets.mnist # Dataset of 60,000 handwritten digits
 (x_train, y_train), (x_test, y_test) = mnist.load_data() # Loads and splits data (90:10)
+
+x_train = tf.keras.utils.normalize(x_train, axis=1) # Scales down x data to 0-10
+x_test = tf.keras.utils.normalize(x_test, axis=1)
